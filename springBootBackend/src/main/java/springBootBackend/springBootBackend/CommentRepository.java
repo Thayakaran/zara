e4a3d240de4219f58d55de577fcd924f8b545f3b
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2018-09-22 16:50:36 +0530

    Add Springboot backend

---
+++ springBootBackend/src/main/java/springBootBackend/springBootBackend/CommentRepository.java
@@ -0,0 +1,9 @@
+package springBootBackend.springBootBackend;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends MongoRepository<Comment, String> {
+
+}
